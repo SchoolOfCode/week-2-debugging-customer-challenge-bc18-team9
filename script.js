4486bbbf1d0dbc76bb187b7320ca73362b054cd8
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-04 15:40:32 +0000

    Fix #5
    Changed from randomNumber to score

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ while (playAgain === false)
   playAgain = confirm("Do you want to play again?");
 
   if (!playAgain); {
-    alert(`Game over. Your final score is ${randomNumber}.`);
+    alert(`Game over. Your final score is ${score}.`);
   }
 }
 
